bb2ff3743a7bb114247b23ef89f53091a383bf0c
coderisgolden <goldencoder@mail.com>
2026-02-08 23:00:45 +0100

    Language3

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -238,7 +238,7 @@ useEffect(() => {
               <DropdownMenuContent className="w-56" align="end" forceMount>
                 <DropdownMenuLabel className="font-normal">
                   <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">Min Profil</p>
+                    <p className="text-sm font-medium leading-none">My Profile</p>
                     <p className="text-xs leading-none text-muted-foreground">
                       {user?.email}
                     </p>
